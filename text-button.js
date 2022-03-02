d59da3127a94e4fe0f6567aa27fa453cb7fb9442
nata ts <tsarnat29@gmail.com>
2022-03-02 21:49:33 +0200

    adding my files

---
+++ text-button.js
@@ -0,0 +1,6 @@
+[
+	{"Житло. Україна та Захід": "https://arthurzekun.notion.site/7b55f03a094c4517b39302df0"},
+	{"Житло. Західна Україна": "https://www.facebook.com/groups/1621686668179225"},
+	{"Транспорт для виїзду або для допомоги": "https://www.ukrainenow.org/refuge"},
+	{"Інфо-атака на рашку": "https://www.change.org/p/%D0%B8%D0%BD%D0%B8%D1%86%D0%B8%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%B8%D0%BC%D0%BF%D0%B8%D1%87%D0%BC%D0%B5%D0%BD%D1%82-%D0%BF%D1%80%D0%B5%D0%B7%D0%B8%D0%B4%D0%B5%D0%BD%D1%82%D0%B0-%D1%80%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B9-%D1%84%D0%B5%D0%B4%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8-%D0%B2%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%B0-%D0%B2%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B8%D1%87%D0%B0-%D0%BF%D1%83%D1%82%D0%B8%D0%BD%D0%B0?fbclid=IwAR2WN5EyIJaeWxpywG2slD2QM4ufqeHM7z76vFxoOISlwdAV2k4cc9f-6Ek"},
+]
